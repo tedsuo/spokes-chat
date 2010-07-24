e08869c6943e9664ca7e6bd98c513287fb6b40ec
tedsuo <ted@radicaldesigns.org>
2010-07-23 23:43:48 -0700

    cleaded up server.js cruft

--- server.js
+++ server.js
@@ -4,27 +4,7 @@ var http = require('http'),
 		io = require('./lib/Socket.IO-node/lib/socket.io'),
 		sys = require('sys'),
 
-chat_server = http.createServer(function(req,res){
-		var path = url.parse(req.url).pathname;
-
-/*
-    if (/\.(js|html|swf)$/.test(path)){
-				try {
-					var swf = path.substr(-4) === '.swf';
-					res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
-					res.write(fs.readFileSync(__dirname + path, swf ? 'binary' : 'utf8'), swf ? 'binary' : 'utf8');
-					res.end();
-				} catch(e){ 
-					send404(res);
-				}
-		} else {
-		
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-  	  res.end('<h1>Hello world</h1>');
-
-		}
-*/
-});
+chat_server = http.createServer(function(req,res){});
 
 chat_server.listen(8080);
 
